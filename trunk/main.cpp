1f53a1c51c9c19b9e00d7ad76d5e49ddec6f59d9
silveira.kaue@gmail.com <silveira.kaue@gmail.com@a508fdfe-0740-11df-a5ab-b9d58601d7b2>
2010-02-03 20:07:25 +0000

    Radio funcionando

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -2,6 +2,7 @@
 
 #include "ssl_client.h"
 #include "ssl_server.h"
+#include "radio.h"
 
 RoboCupSSLClient aitoradio(PORT_AI_TO_RADIO, IP_AI_TO_RADIO);
 
@@ -30,8 +31,36 @@ int main()
 	printf("Press <Enter> to open connection with client...\n");
 	getchar();
 	//TODO: connection with the real robots
+	Radio radio;
+	radio.conecta();
 
 	while(1) {
+		//robotNumber, motorForces, drible, kick
+		char a;
+		int forces[4];
+
+		/*
+		a = getchar();
+		if(a == '1')
+		{
+			for(int i=0; i<4; i++)
+				forces[i] = 30;
+		}
+		else if(a == '0')
+		{
+			for(int i=0; i<4; i++)
+				forces[i] = 0;
+		}
+		else if(a == '2')
+		{
+			forces[0] = 30;
+			forces[2] = -30;
+			forces[1] = 0;
+			forces[3] = 0;
+		}
+		*/
+
+		radio.send(3, forces, 0, 0);
 		receive();
 		send();
 	}
