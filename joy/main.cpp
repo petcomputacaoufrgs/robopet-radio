cea2c92ed51e1d8b261816e5798e50f0c35aaeed
garciadaudt <garciadaudt@a508fdfe-0740-11df-a5ab-b9d58601d7b2>
2010-10-29 17:31:54 +0000

    little joy

--- joy/main.cpp
+++ joy/main.cpp
@@ -10,8 +10,14 @@
 #include <stdio.h>
 #include <math.h>
 #include "joy.h"
+#include "rp_server.h"
+#include "vector.h"
 
 Joystick global_joy;
+
+RoboPETServer joyToRadio(PORT_AI_TO_RADIO, IP_AI_TO_RADIO);
+int current_bot = 0;
+
 
 void SetupRC(void)
 {
@@ -19,7 +25,7 @@ void SetupRC(void)
 		{
 			global_joy.loadConfig();
 		}
-		
+
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
@@ -28,20 +34,52 @@ void RenderScene(void)
      glutSwapBuffers();
 }
 
+
+
+void sendToRadio(RoboPETServer joyToRadio) {
+
+    RoboPET_WrapperPacket packet;
+    AIToRadio *aitoradioPacket = packet.mutable_aitoradio();
+
+    AIToRAdio::Robot *r = aitoradioPacket->add_robots();
+
+    RP::Vector disp(global_joy.getX(), global_joy.getY());
+    disp.normalizeMe();
+
+    float disp_theta = 360 * global_joy/1000;
+
+    r->set_displacement_x(disp.getX());
+    r->set_displacement_y(disp.getY());
+    r->set_displacement_theta(disp_theta);
+
+    r->set_kick(0);
+    r->set_drible(0);
+    r->set_id(current_bot);
+    r->set_current_theta(0);
+
+    aitoradio.send(packet);
+
+}
+
 void JoystickFunc(unsigned int mask, int x, int y, int z)
 {
-	
+
 		global_joy.receiveInput(mask,x,y,z);
-		
+
 		vector<int> vectorzenyo = global_joy.getAll();
 		for(unsigned int i = 0; i < vectorzenyo.size(); ++i)
-			cout << vectorzenyo[i] << " - ";
+			cout << vectorzenyo[i] << " | ";
 		cout << endl;
 		global_joy.printStatus();
+
+        sendToRAdio(joyToRadio);
 }
 
 int main(int argc, char **argv)
 {
+
+    joyToRadio.open();
+
 
 	//incializa a glut para funcionar no windows e no linux
     GAMBI_INIT();
@@ -56,7 +94,6 @@ int main(int argc, char **argv)
     glutJoystickFunc(JoystickFunc,100);
     SetupRC();
     glutMainLoop();
-		
+
     return 0;
 }
-
