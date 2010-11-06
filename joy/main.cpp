ffef40bb7c2a6b0db3869d999095500638e3e6f3
ffsbeltrao <ffsbeltrao@a508fdfe-0740-11df-a5ab-b9d58601d7b2>
2010-11-06 15:18:51 +0000

    Commiting all robopet. joy working fully now, some changes in the communication

--- joy/main.cpp
+++ joy/main.cpp
@@ -10,10 +10,17 @@
 #define INC 1
 #define DEC -1
 
+#define MAX_BOT 5
+#define MAX_MOTOR 3
+#define MAX_BOT_INDEX MAX_BOT - 1
+#define MAX_MOTOR_INDEX MAX_MOTOR - 1
+
 Joystick global_joy;
 
-RoboPETServer joyToRadio(PORT_AI_TO_RADIO, IP_AI_TO_RADIO);
+RoboPETServer joyToRadio(PORT_JOY_TO_RADIO, IP_JOY_TO_RADIO);
 int current_bot = 0;
+int current_motor = 0;
+int forces[5][3] = {{0}};
 
 
 void SetupRC(void)
@@ -31,7 +38,7 @@ void RenderScene(void)
      glutSwapBuffers();
 }
 
-double calcCurrentTheta() {
+double calcAnalogicTheta() {
 
 	double theta=0;
 
@@ -59,7 +66,8 @@ void sendToRadio() {
 
     RoboPET_WrapperPacket packet;
 
-    AIToRadio *aitoradioPacket = packet.mutable_aitoradio();
+    JoyToRadio *joytoradioPacket = packet.mutable_joytoradio();
+	AIToRadio *aitoradioPacket = joytoradioPacket->mutable_aitoradio();
 
     AIToRadio::Robot *r = aitoradioPacket->add_robots();
 
@@ -67,19 +75,25 @@ void sendToRadio() {
     disp.normalizeMe();
 
     float disp_theta = 360 * global_joy.getZ()/1000;
+    float analogic_angle = calcAnalogicTheta();
 
-    r->set_displacement_x(0);
+    r->set_displacement_x(cos(analogic_angle*3.1415/180));
 	//this crazy test is for us to determine we are pressing or not the direction button in the joystick
 	//if not, we don't move the bot
-    r->set_displacement_y((abs(global_joy.getX()) == 1000 ||
-						   abs(global_joy.getY()) == 1000  ) ?	1 : 0);
+    r->set_displacement_y(sin(analogic_angle*3.1415/180));
     r->set_displacement_theta(disp_theta);
 
 	r->set_kick(global_joy.isPressed(KICK));
-    r->set_drible(global_joy.isPressed(PASS));
+    r->set_drible(global_joy.isPressed(DRIBBLE));
+    r->set_chip_kick(global_joy.isPressed(CHIP_KICK));
     r->set_id(current_bot);
 
-	r->set_current_theta(calcCurrentTheta());
+	r->set_current_theta(0);
+	
+	joytoradioPacket->set_force_0(forces[current_bot][0]);
+	joytoradioPacket->set_force_1(forces[current_bot][1]);
+	joytoradioPacket->set_force_2(forces[current_bot][2]);
+	joytoradioPacket->set_secret_attack(global_joy.isPressed(TATSUMAKI_SENPUU_KYAKU));
 
     joyToRadio.send(packet);
 
@@ -89,15 +103,34 @@ void changeBot(int sinal) {
 
 	//if we are with the first or last bot, we do nothing
 	if(sinal < 0 && current_bot == 0) {}
-	else if(sinal > 0 && current_bot == 4) {}
+	else if(sinal > 0 && current_bot == MAX_BOT_INDEX) {}
 
-	else current_bot += sinal;
+	else {
+		current_bot += sinal;
+		current_motor = 0;
+	}
+}
+
+void changeMotor(int sinal) {
+	
+	//if we are with the first or last bot, we do nothing
+	if(sinal < 0 && current_motor == 0) {}
+	else if(sinal > 0 && current_motor == MAX_MOTOR_INDEX) {}
+
+	else current_motor += sinal;
+}
+
+void changeForce(int sinal) {
+	
+	forces[current_bot][current_motor] += sinal;
 }
 
 void JoystickFunc(unsigned int mask, int x, int y, int z)
 {
 
 	cout << endl << endl << "Robot " << current_bot << endl;
+	cout << "Motor: " << current_motor << "Ajuste fino<" << forces[current_bot][0] << "," << forces[current_bot][1] << "," << forces[current_bot][2] << ">\n";
+		
 	global_joy.receiveInput(mask,x,y,z);
 
 	vector<bool> buttons = global_joy.getButtonsPressed();
@@ -106,12 +139,18 @@ void JoystickFunc(unsigned int mask, int x, int y, int z)
 	cout << endl;
 	global_joy.printStatus();
 
-	//SHORYUKEN and BRAKE are the names of the buttons -> joy.conf file
-	if(buttons[SHORYUKEN])
+	if(buttons[INC_BOT])
 		changeBot(INC);
-	if(buttons[BRAKE])
+	if(buttons[DEC_BOT])
 		changeBot(DEC);
-
+	if(buttons[INC_MOTOR])
+		changeMotor(INC);
+	if(buttons[DEC_MOTOR])
+		changeMotor(DEC);
+	if(buttons[INC_FORCE])
+		changeForce(INC);
+	if(buttons[DEC_FORCE])
+		changeForce(DEC);
 }
 
 void idleFunc() {
--- main.cpp
+++ main.cpp
@@ -20,14 +20,16 @@ typedef struct
 	float force_y;
 	float displacement_theta;
 	int kick;
+	int chip_kick;
 	int drible;
 	float current_theta;
 	int motorForces[4];
 	int id;
+	int secret_attack;
 } Robot;
 
 void sendToTracker();
-void sendToRobots();
+void sendToRobots(bool toRadio);
 void sendToSimulator();
 
 
@@ -36,20 +38,23 @@ void sendToSimulator();
 //RoboPETServer radiototracker(PORT_RADIO_TO_TRACKER, IP_RADIO_TO_TRACKER);
 RoboPETServer radiotosim(PORT_RADIO_TO_SIM, IP_RADIO_TO_SIM);
 RoboPETClient aitoradio(PORT_AI_TO_RADIO, IP_AI_TO_RADIO);
+RoboPETClient joytoradio(PORT_JOY_TO_RADIO, IP_JOY_TO_RADIO);
 
 bool real_radio = true;
 
 int CLOCK_WISE_VELOCITY = 15,
     COUNTER_CLOCK_WISE_VELOCITY = 16,
-    MIN_DIFF = 40,
+    MIN_DIFF = 30,
 	MAX_FORCE = 127;
 
-bool DEBUG = false;
+bool DEBUG = true;
 int robot_total = NUM_ROBOTS;
 int team_id;
 
 Robot robots[NUM_ROBOTS];
 RadioUSB radio;
+
+int ajusteFino[5][3] = {{0}};
 
 // >>> END OF GLOBALS
 
@@ -64,32 +69,46 @@ void giraAnda(int robotIndex)
 	Vector normal(cos(robots[robotIndex].current_theta*3.1415/180), sin(robots[robotIndex].current_theta*3.1415/180));
 
 
-	float angle = desl.angle(normal)*180/3.1415;
+	float angle = desl.angleDegrees(normal);
 
+	printf("Trague-o: %f,%f\n", normal.getX(), normal.getY());
 	printf("Angle %f\n", angle);
 
 	 if(angle < MIN_DIFF) {
-		//printf("ahead ahoy!\n");
+		printf("ahead ahoy!\n");
 		robots[robotIndex].motorForces[0] = MAX_FORCE*6/10;
         robots[robotIndex].motorForces[1] = 0;
         robots[robotIndex].motorForces[2] = (MAX_FORCE*6/10) | 128;
 	}
-	else if(angle > MIN_DIFF && angle < 180) {
+	else if(angle > MIN_DIFF && angle <= 180) {
 		//horario
-		//printf("horario\n");
+		printf("horario\n");
 		for(int i=0; i < 3; i++)
-			robots[robotIndex].motorForces[i] = int(MAX_FORCE*0.5/10) | 128;
+			robots[robotIndex].motorForces[i] = int(MAX_FORCE*2/10) | 128;
 	}
-	else {
+	else if(angle > MIN_DIFF && angle > 180) {
 		//antihorario mano
-		//printf("antihorario\n");
+		printf("antihorario\n");
 		for(int i=0; i < 3; i++)
 			robots[robotIndex].motorForces[i] = MAX_FORCE*2/10;
 	}
+	else {
+		//parado ai
+		printf("paradin\n");
+		for(int i=0; i < 3; i++)
+			robots[robotIndex].motorForces[i] = 0;
+	}
+	
+	for(int i = 0; i < 3; i++)
+		robots[robotIndex].motorForces[i] += ajusteFino[robotIndex][i];
+	
+	if(robots[robotIndex].secret_attack)
+		for(int k = 0; k < 3; k++)
+			robots[robotIndex].motorForces[k] = MAX_FORCE;
 }
 
-void receive()
-{
+void receiveFromAI() {
+	
 	RoboPET_WrapperPacket packet;
 	if (aitoradio.receive(packet) && packet.has_aitoradio()) {
 		printf("----------------------------");
@@ -107,16 +126,68 @@ void receive()
 			robots[i].force_y = packet.aitoradio().robots(i).displacement_y();
 			robots[i].displacement_theta = packet.aitoradio().robots(i).displacement_theta();
 			robots[i].kick = packet.aitoradio().robots(i).kick();
+			robots[i].chip_kick = packet.aitoradio().robots(i).chip_kick();
 			robots[i].drible = packet.aitoradio().robots(i).drible();
 			robots[i].id = packet.aitoradio().robots(i).id();
 			robots[i].current_theta = packet.aitoradio().robots(i).current_theta();
 
 			if(DEBUG)
-				printf("RECEIVE Robot %d: <%f,%f,%f> (%f degrees) (Kick = %d) (Drible = %d)\n", robots[i].id, robots[i].force_x, robots[i].force_y, robots[i].current_theta, robots[i].displacement_theta, robots[i].kick, robots[i].drible);
+				printf("RECEIVE Robot %d: <%f,%f,%f> (%f degrees) (Kick = %d) (Drible = %d) (Chip Kick = %d)\n", robots[i].id, robots[i].force_x, robots[i].force_y, robots[i].current_theta, robots[i].displacement_theta, robots[i].kick, robots[i].drible, robots[i].chip_kick);
 		}
 	}
 	else
 		printf("Didn't receive  AI-To-Radio.\n");
+}
+
+void receiveFromJoy() {
+	
+	RoboPET_WrapperPacket packet;
+	if(joytoradio.receive(packet) && packet.has_joytoradio()) {
+		
+		printf("----------------------------");
+		printf("Received Joy-To-Radio!\n");
+
+		AIToRadio aitoradio = packet.joytoradio().aitoradio();
+		robot_total = aitoradio.robots_size();
+
+		//GAMBIARRA PARA DEIXAR ECP FELIZ
+		if (robot_total < 0)
+			robot_total = NUM_ROBOTS;
+
+		for(int i=0; i<aitoradio.robots_size() && i<NUM_ROBOTS; i++)
+		{
+			robots[i].force_x = aitoradio.robots(i).displacement_x();
+			robots[i].force_y = aitoradio.robots(i).displacement_y();
+			robots[i].displacement_theta = aitoradio.robots(i).displacement_theta();
+			robots[i].kick = aitoradio.robots(i).kick();
+			robots[i].chip_kick = aitoradio.robots(i).chip_kick();
+			robots[i].drible = aitoradio.robots(i).drible();
+			robots[i].id = aitoradio.robots(i).id();
+			robots[i].current_theta = aitoradio.robots(i).current_theta();
+			
+			ajusteFino[robots[i].id][0] = packet.joytoradio().force_0();
+			ajusteFino[robots[i].id][1] = packet.joytoradio().force_1();
+			ajusteFino[robots[i].id][2] = packet.joytoradio().force_2();
+			
+			robots[i].secret_attack = packet.joytoradio().secret_attack();
+
+			if(DEBUG) {
+				printf("RECEIVE Robot %d: <%f,%f,%f> (%f degrees) (Kick = %d) (Drible = %d) (Chip Kick = %d)\n", robots[i].id, robots[i].force_x, robots[i].force_y, robots[i].current_theta, robots[i].displacement_theta, robots[i].kick, robots[i].drible, robots[i].chip_kick);
+				printf("Ajuste Fino: <%i,%i,%i>\n", ajusteFino[robots[i].id][0], ajusteFino[robots[i].id][1], ajusteFino[robots[i].id][2]);
+				if(robots[i].secret_attack)
+					printf("\nATAQUE DAS CORUJA !!!\n");
+			}
+		}
+
+	}
+	else
+		printf("Didn't receive  Joy-To-Radio.\n");
+}
+
+void receive()
+{
+	receiveFromAI();
+	receiveFromJoy();
 }
 
 
@@ -160,11 +231,11 @@ void send()
 {
 	//sendToTracker();
 	//sendToSim();
-	sendToRobots();
+	sendToRobots(real_radio);
 }
 
 
-void sendToRobots()
+void sendToRobots(bool toRadio)
 {
 	/*
 	0000 0000 (0%) - 1111 1111 (100%) - 1000 0000 (50%)
@@ -185,11 +256,12 @@ void sendToRobots()
 
 		if(DEBUG)
 		{
-			printf("SENDING Robot %d: <%f, %f> (%f degrees) (Kick = %d) (Drible = %d)\n",
-			i, 
+			printf("SENDING Robot %d: <%f, %f> (%f degrees) (Kick = %d) (Drible = %d) (Chip Kick = %d)\n",
+			robots[i].id, 
 			robots[i].force_x,
 			robots[i].force_y, robots[i].displacement_theta,
-			robots[i].kick, robots[i].drible);
+			robots[i].kick, robots[i].drible,
+			robots[i].chip_kick);
 
 			printf("%d = ", robots[i].id+1);
 
@@ -200,7 +272,7 @@ void sendToRobots()
 			printf("\n");
 		}
 
-		data_send[i*NUM_ROBOTS    ] = robots[i].id;
+		data_send[i*NUM_ROBOTS    ] = robots[i].id+11;
 		data_send[i*NUM_ROBOTS + 1] = robots[i].motorForces[0];
 		data_send[i*NUM_ROBOTS + 2] = robots[i].motorForces[1];
 		data_send[i*NUM_ROBOTS + 3] = robots[i].motorForces[2];
@@ -208,8 +280,10 @@ void sendToRobots()
 
 	}
 
-	radio.usbSendData( data_send, WRITE_BYTE_NUMBER );
-	usleep(SLEEP_TIME);
+	if(toRadio) {
+		radio.usbSendData( data_send, WRITE_BYTE_NUMBER );
+		usleep(SLEEP_TIME);
+	}
 
 }
 
@@ -248,6 +322,7 @@ void initialize()
     radio.usbInitializeDevice();
 
 	aitoradio.open(false);
+	joytoradio.open(false);
 
 	printf("Press <Enter> to open connection with client...\n");
 	getchar();
@@ -313,7 +388,7 @@ int main(int argc, char **argv)
 	    }
 		rewindscr();
 		receive();
-		//send();
+		send();
 	}
 
     while(1) {
@@ -324,7 +399,7 @@ int main(int argc, char **argv)
 				robots[i].motorForces[j] = 0;
 
             for(int k = 0; k < 1000; k++)
-				sendToRobots();
+				sendToRobots(real_radio);
 	    }
 	}
 
